6aa786b1263910078fee93832d9493f640c11d58
sptcse123 <sptcse123@gmail.com>
2024-03-13 10:27:15 +0530

    c1

---
+++ abc.java
@@ -0,0 +1,8 @@
+public class hello
+  {
+              public static coid main (String [] args)
+                          {
+                                        System.out.println("Hello world");
+                                            }
+  }
+
